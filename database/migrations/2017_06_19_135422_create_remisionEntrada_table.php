22671c4078f691ad75b99682a68f939f84875f81
juan david <juanvc123@hotmail.com>
2017-06-19 09:01:01 -0500

    se crearon las migraciones de tablas

---
+++ database/migrations/2017_06_19_135422_create_remisionEntrada_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRemisionEntradaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('remision_entrada', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('codigo', 20);
+            $table->date('fecha_documento');
+            $table->integer('idProveedor')->unsigned();
+            $table->integer('idAlmacen')->unsigned();
+            $table->foreign('idProveedor')->references('id')->on('proveedores');
+            $table->foreign('idAlmacen')->references('id')->on('almacenes');
+            $table->integer('estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('remision_entrada');
+    }
+}
