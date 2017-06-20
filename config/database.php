c77877eb1be49e890677fe806d1ae87e8fd4785c
juan david <juanvc123@hotmail.com>
2017-06-20 05:09:47 -0500

    .

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => 'mer.cqfsx30cjean.us-west-2.rds.amazonaws.com',
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => 'mer_database',
+            'username' => 'mer_user',
+            'password' => 'mer_password',
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
